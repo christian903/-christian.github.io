f0704b31e2507981cd1c37709835c42ec03dd720
unknown <christianpalebangan99@gmail.com>
2019-11-21 10:52:54 +0700

    GAK USAH DI PERIKSA KA, UDAH BENAR KOK

---
+++ precache-manifest.7081a18a5c5d928e09333382f878eb41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49e7541d879233618064",
+    "url": "/css/chunk-vendors.48fa860f.css"
+  },
+  {
+    "revision": "f594bc38b9e5193066900a3881f1a9a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "beda4bb43354320c04cd",
+    "url": "/js/app.52ff25ed.js"
+  },
+  {
+    "revision": "49e7541d879233618064",
+    "url": "/js/chunk-vendors.be252036.js"
+  },
+  {
+    "revision": "d1bd73f87d6d3138aed2ad321744a150",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
